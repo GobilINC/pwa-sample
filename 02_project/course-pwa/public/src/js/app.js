a39a6a73418dc9ec37406970abd92a888d536601
david <dcarnegie18@gmail.com>
2019-03-11 00:09:20 +0300

    upload level 55 done

--- 02_project/course-pwa/public/src/js/app.js
+++ 02_project/course-pwa/public/src/js/app.js
@@ -1,6 +1,10 @@
 
 var deferredPrompt;
 
+if (!window.Promise) {
+  window.Promise = Promise;
+}
+
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
     .register('/sw.js')
@@ -26,6 +30,52 @@ var promise = new Promise(function(resolve, reject) {
     //console.log('This is executed once the timer is done!');
   }, 3000);
 });
+
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://httpbin.org/ip');
+xhr.responseType = 'json';
+
+xhr.onload = function() {
+  console.log(xhr.response);
+};
+
+xhr.onerror = function() {
+  console.log('Error!');
+};
+
+xhr.send();
+
+fetch('https://httpbin.org/ip')
+  .then(function(response) {
+    console.log(response);
+    return response.json();
+  })
+  .then(function(data) {
+    console.log(data);
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
+
+fetch('https://httpbin.org/post', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  },
+  mode: 'cors',
+  body: JSON.stringify({message: 'Does this work?'})
+})
+  .then(function(response) {
+    console.log(response);
+    return response.json();
+  })
+  .then(function(data) {
+    console.log(data);
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
 
 // promise.then(function(text) {
 //   return text;
